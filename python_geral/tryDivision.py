db5b14fff449883230691a983a68a23351025ebc
Wedny Fernandes <wedny.fernandes@gmail.com>
2025-10-16 21:32:31 -0300

    16/10 - final aula

--- python_geral/tryDivision.py
+++ python_geral/tryDivision.py
@@ -1,8 +1,9 @@
-num1 = int(input("Qual valor?: "))
-num2 = int(input("Dividido pro quanto?: "))
-
 try:
+    num1 = int(input("Qual valor?: "))
+    num2 = int(input("Dividido pro quanto?: "))
     final = num1 / num2
     print(f"{num1} dividido por {num2}: {final}")
 except ZeroDivisionError:
     print("Não é possivel dividir por zero")
+except ValueError:
+    print("Você digitou um número inválido ou não número")
